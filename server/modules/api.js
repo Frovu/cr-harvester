e296a6ceaf3c9ad538c15447defa221c0085763c
Frovu <sfrovis@gmail.com>
2021-02-08 15:04:16 +0300

    add: api.js

---
+++ server/modules/api.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('sections/', (req, res) => {
+	return res.sendStatus(501);
+});
+
+router.get('data/', (req, res) => {
+	return res.sendStatus(501);
+});
+
+router.post('data/', (req, res) => {
+	return res.sendStatus(501);
+});
+
+module.exports = router;
