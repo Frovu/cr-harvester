8d22cf229515fa2168f100e6cd320c27c59f9048
Frovu <sfrovis@gmail.com>
2021-02-10 15:56:51 +0300

    fix: endpoints

--- server/modules/api.js
+++ server/modules/api.js
@@ -2,18 +2,18 @@ const express = require('express');
 const db = require('./database');
 const router = express.Router();
 
-router.get('sections/', (req, res) => {
+router.get('/sections', (req, res) => {
 	// hide devices authority keys
 	const sections = db.getSections();
 	for(const s in sections) sections[s].key = undefined;
 	return res.status(200).json(Object.values(sections));
 });
 
-router.get('data/', (req, res) => {
+router.get('/data', (req, res) => {
 	return res.sendStatus(501);
 });
 
-router.post('data/', (req, res) => {
+router.post('/data', (req, res) => {
 	return res.sendStatus(501);
 });
 
