c4e77bf49f84a4efb35270e325036390a8c07501
Frovu <sfrovis@gmail.com>
2021-02-10 20:28:39 +0300

    add: some post data tests

--- server/__tests__/api.ut.js
+++ server/__tests__/api.ut.js
@@ -1,7 +1,9 @@
 /* eslint-disable no-undef, no-unused-vars*/
-global.log = () => {};
+global.log = (a) => {console.log(a)};
 const db = require('../modules/database');
+db.pool.query = jest.fn(()=>{});
 jest.spyOn(db, 'connect').mockImplementation(()=>{});
+jest.spyOn(db, 'validate').mockImplementation(d=>d.k === 'valid_key');
 jest.spyOn(db, 'getSections').mockImplementation(()=>{return {
 	'akey': {key: 'akey', asd: 'asd'}
 };});
@@ -16,12 +18,32 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', require('../modules/api.js'));
 
 describe('api', () => {
-	describe('GET sections', () => {
+	describe('GET /sections', () => {
 		it('responds with 200', async () => {
 			const res = await request(app).get('/api/sections');
 			expect(res.status).toEqual(200);
 		});
 		it('does not show auth keys', async () => {
+			const res = await request(app).get('/api/sections');
+			const b = res.body;
+			expect(b).toEqual([{asd: 'asd'}]);
+		});
+	});
+	describe('POST /data', () => {
+		const data = 'dt=1970-01-01T00:00:00&t=13.37&p=13.38&c=1,2,3,4&k=valid_key';
+		it('responds 400 to malformed or unauthorized data', async () => {
+			let res = await request(app).post('/api/data').send('t=1');
+			expect(res.status).toEqual(400);
+			res = await request(app).post('/api/data').send('t=1&k=valid_key');
+			expect(res.status).toEqual(500);
+			res = await request(app).post('/api/data').send('t=1&c=1,2&k=valid_key');
+			expect(res.status).toEqual(500);
+		});
+		it('translates values correctly', async () => {
+			let res = await request(app).post('/api/data').send(data);
+			expect(res.status).toEqual(200);
+		});
+		xit('does not show auth keys', async () => {
 			const res = await request(app).get('/api/sections');
 			const b = res.body;
 			expect(b).toEqual([{asd: 'asd'}]);
