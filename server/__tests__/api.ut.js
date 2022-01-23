9c7c3ca4b640ee93733853f321d7f634881379b9
Frovu <sfrovis@gmail.com>
2022-01-23 19:06:08 +0300

    add: muon tests

--- server/__tests__/api.ut.js
+++ server/__tests__/api.ut.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-undef, no-unused-vars*/
-global.log = (a) => {}; // console.log(a);
+global.log = (a) => { };
+// global.log = (a) => { console.log(a); };
 const db = require('../modules/database');
 db.pool.query = jest.fn((q, args) => {
-	values = [ 13.37, 13.38, 1, 2, new Date('1970-01-01T00:00:00.000Z')];
+	if (args[0] == 87) { // muon
+		return q.includes('muon_data') && q.includes('n_v');
+	}
+	const values = [ 13.37, 13.38, 1, 2, new Date('1970-01-01T01:00:00.000Z')];
 	for (i=0; i<args.length; ++i) {
 		if ((i != 4) ? (args[i] != values[i]) : args[i].getTime() != values[i].getTime())
 			return false;
@@ -15,6 +19,7 @@ jest.spyOn(db, 'connect').mockImplementation(()=>{});
 // 	'akey': {key: 'akey', asd: 'asd', type: 'nm'}
 // };});
 db.devices.valid_key = {key: 'valid_key', channels: 12, type: 'nm'};
+db.devices.muon_key = {key: 'muon_key', channels: 3, type: 'muon'};
 
 require('dotenv').config();
 const express = require('express');
@@ -38,11 +43,10 @@ describe('api', () => {
 		});
 	});
 	describe('POST /data', () => {
-		const data = 'dt=1970-01-01T00:00:00Z&t=13.37&p=13.38&c[]=1&c[]=2&k=valid_key';
 		it('responds 401 to unauthorized data', async () => {
-			let res = await request(app).post('/api/data').send('dt=1970-01-01T00:00:00Z&t=13.37&p=13.38&c[]=1&c[]=2');
+			let res = await request(app).post('/api/data').send('dt=3600&t=13.37&p=13.38&c[]=1&c[]=2');
 			expect(res.status).toEqual(401);
-			res = await request(app).post('/api/data').send('dt=1970-01-01T00:00:00Z&t=13.37&p=13.38&c[]=1&c[]=2&t=1&k=invaliddd');
+			res = await request(app).post('/api/data').send('dt=3600&t=13.37&p=13.38&c[]=1&c[]=2&t=1&k=invaliddd');
 			expect(res.status).toEqual(401);
 			expect(db.pool.query).not.toHaveBeenCalled();
 		});
@@ -56,7 +60,18 @@ describe('api', () => {
 			expect(db.pool.query).not.toHaveBeenCalled();
 		});
 		it('translates values correctly', async () => {
+			const data = 'dt=1970-01-01T01:00:00Z&t=13.37&p=13.38&c[]=1&c[]=2&k=valid_key';
+			const data0 = 'dt=3600&t=13.37&p=13.38&c[]=1&c[]=2&k=valid_key';
 			let res = await request(app).post('/api/data').send(data);
+			expect(res.status).toEqual(200);
+			expect(db.pool.query).toHaveLastReturnedWith(true);
+			res = await request(app).post('/api/data').send(data0);
+			expect(res.status).toEqual(200);
+			expect(db.pool.query).toHaveLastReturnedWith(true);
+		});
+		it('switches table for muon data', async () => {
+			const muon_data = 'dt=1970-01-01T01:00:00Z&t=87&p=13.38&c[]=1&c[]=1&c[]=4&k=muon_key';
+			let res = await request(app).post('/api/data').send(muon_data);
 			expect(res.status).toEqual(200);
 			expect(db.pool.query).toHaveLastReturnedWith(true);
 		});
