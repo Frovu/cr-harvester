dad1c80ba7683c4b44934fe5f0ea5f6c2dc37273
Frovu <sfrovis@gmail.com>
2022-09-08 17:48:15 +0300

    add: updated seconds ago

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import DevicePane from './DevicePane';
 import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
@@ -14,28 +14,51 @@ export default function App() {
 	);
 };
 
+function StatusInfo(props) {
+	const [ago, setAgo] = useState('just now');
+	useEffect(() => {
+		if (!props.data) return;
+		const timeout = setInterval(() => {
+			const sec = Math.floor((Date.now() - props.dataUpdatedAt) / 1000);
+			setAgo(`${sec} second${sec===1?'':'s'} ago`);
+		}, 1000);
+		return () => {
+			console.log('clear')
+			setInterval(timeout);
+		};
+	}, [props.data, props.dataUpdatedAt]);
+
+	let text = '';
+	if (props.isLoading)
+		text = 'Loading...';
+	else if (props.isFetching)
+		text = 'Fetching...';
+	else if (props.error)
+		text = `Error: ${props.error.message}`;
+	else
+		text = `Updated ${ago}.`;
+
+	return <div className="Info">{text}</div>
+}
+
 function StatusPanes(props) {
-	const { isLoading, error, data, isFetching } = useQuery('stats', async () => {
-		const query = new URLSearchParams({ limit: props.rowLimit || DEFAULT_LIMIT }).toString();
-		const resp = await fetch(process.env.REACT_APP_API + '/stations?' + query);
+	const query = useQuery('stats', async () => {
+		const para = new URLSearchParams({ limit: props.rowLimit || DEFAULT_LIMIT }).toString();
+		const resp = await fetch(process.env.REACT_APP_API + '/stations?' + para);
 		return await resp.json();
 	});
-
-	if (isLoading) return <span>Loading...</span>
-
-	if (error) return <span>Error: {error.message}</span>
-
+	const data = query.data;
 	return (
 		<>
-			<div>{isFetching ? 'Updating...' : ''}</div>
+			<StatusInfo { ...query } />
 			<div className="App">
-				{Object.keys(data).map(id => (<>
+				{data ? Object.keys(data).map(id => (
 					<DevicePane
 						id = { id }
-						ip={ data[id].ip }
-						data={ data[id].data }
+						ip = { data[id].ip }
+						data = { data[id].data }
 					/>
-				</>))}
+				)) : ''}
 			</div>
 		</>
 	);
