45185fcfb25ddd31083ab3971f9ad349a40ef189
Frovu <sfrovis@gmail.com>
2022-09-09 15:47:32 +0300

    add: menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
-import './css/App.css';
+import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
+
 import StatusTab from './StatusTab';
+
+import './css/App.css';
 
 const queryClient = new QueryClient();
 
-export default function App() {
+function Menu({ onChange, activeTab }) {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<div className="App">
-				<StatusTab />
-			</div>
-		</QueryClientProvider>
+		<div className="Menu">
+			{['Status', 'Corrections', 'Subscriptions'].map(tab => (
+				<div className="MenuButton">
+					<label>
+						<input type="radio" name="Menu" value={tab} checked={activeTab===tab} onChange={onChange}/>
+						{tab}
+					</label>
+				</div>
+			))}
+		</div>
+	);
+}
+
+export default function App() {
+	const [activeTab, setActiveTab] = useState('Status');
+	return (
+		<>
+			<Menu activeTab={activeTab} onChange={e => setActiveTab(e.target.value)}/>
+			<QueryClientProvider client={queryClient}>
+				<div className="App">
+					<StatusTab />
+				</div>
+			</QueryClientProvider>
+		</>
 	);
 };
