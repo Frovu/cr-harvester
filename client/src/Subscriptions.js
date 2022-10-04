e4b0fef84aaabe174bcfe4e0e08e2271fb879870
Frovu <sfrovis@gmail.com>
2022-10-04 13:34:21 +0300

    fix: minor

--- client/src/Subscriptions.js
+++ client/src/Subscriptions.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useQueryClient, useQuery, useMutation } from 'react-query';
 
 import './css/Subscriptions.css';
