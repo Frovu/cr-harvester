75a0991066100381241b38a9d7ad0dbaf15b8e47
Frovu <sfrovis@gmail.com>
2022-10-26 19:44:36 +0300

    fix: default tab

--- client/src/App.js
+++ client/src/App.js
@@ -23,7 +23,7 @@ function Menu({ onChange, activeTab }) {
 }
 
 function App() {
-	const [activeTab, setActiveTab] = useState('Corrections'); // FIXME:
+	const [activeTab, setActiveTab] = useState('Status');
 	const secretRef = useRef();
 	const query = useQuery('stations',
 		() => fetch((process.env.REACT_APP_API || '') + 'api/stations').then(res => res.json()));
